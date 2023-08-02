928399d785132e8a8da5040278522bb777a25db4
Lasisi Abayomi <bmlasisi@gmail.com>
2023-08-02 20:59:57 +0100

    updated footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -36,14 +36,14 @@ const Footer = () => (
       </div>
       <div className="gpt3__footer-links_div">
         <h4>Get in touch</h4>
-        <p>Crechterwoord K12 182 DK Alknjkcb</p>
+        <p>Steel enterprises</p>
         <p>085-132567</p>
-        <p>info@payme.net</p>
+        <p>info@steelenterprises.net</p>
       </div>
     </div>
 
     <div className="gpt3__footer-copyright">
-      <p>@2021 GPT-3. All rights reserved.</p>
+      <p>@2023 GPT-3. All rights reserved.</p>
     </div>
   </div>
 );
